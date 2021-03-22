b5ee3cc64b230df359bd8b3ad362f12d944649af
Arnoldoliveira <79678515+Arnoldoliveira@users.noreply.github.com>
2021-03-22 19:01:22 -0400

    Add files via upload

---
+++ aula 03/js index.js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(
+    function () {
+        alert(" o documento carregou!")
+    }
+)
+
+$("#bt_inicio").click(
+    function () {
+        alert('opa, voce clicou no botão inicio');
+    }
+)
+
